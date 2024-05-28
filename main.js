0c0e990b4eafd6c4a8c9a7e0ecb42017beda2c2c
Michael Vasquez <michael_s_vasquez@yahoo.com>
2024-05-28 13:49:04 -0500

    added files and code

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
-const materials = ['Hylian Rice', 'Big Hearty Truffle', 'Tabantha Wheat', 'Raw Prime Meat', 'Hateno Cheese', 'Bird Egg']
+/*
+    we have a cook function that has two parameters
+    we want to be able to choose the ingredients in the argument when we invoke the function and get the completed dish
+    an array of objects will be good as we can can have a first and second ingredient property, with a dish property with their associated value
+    we can take the array of objects and store it in a variable and do a for statement 
+
+
+*/
+const materials = [{
+        id: 1,
+        ingredientOne: "Hylian Rice",
+        ingredientTwo: "Big Hearty Truffle",
+        cookedDish: "Mushroom Rice Balls"
+},{
+        id: 2,
+        ingredientOne: "Hateno Cheese",
+        ingredientTwo: "Bird Egg",
+        cookedDish: "Cheesy Omelette"   
+},{
+        id: 3,
+        ingredientOne: "Tabantha Wheat",
+        ingredientTwo: "Hateno Cheese",
+        cookedDish: "Cheesy Hylian Pizza"
+},{
+        id: 4,
+        ingredientOne: "Raw Prime Meat",
+        ingredientTwo: "Hylian Rice",
+        cookedDish: "Prime Meat and Rice Bowl"
+}]
+
+const materialsArray = materials
+
 const meals = []
 
 const cook = (firstIngredient, secondIngredient) => {
-    if (firstIngredient === 'Hylian Rice' && secondIngredient === 'Big Hearty Radish') {
-        meals.push('Mushroom Rice Balls')
+    for (const ingredient of materialsArray) {
+        if (firstIngredient === ingredient.ingredientOne && secondIngredient === ingredient.ingredientTwo) {
+            meals.push(ingredient.cookedDish)
+        }
     }
+    return meals
 }
-
-cook(materials[1], materials[0])
 
 console.log('MEALS:')
 console.log('---------------')
+cook("Raw Prime Meat", "Hylian Rice")
+
 for (const meal of meals) {
-    console.log(meel)
+    console.log(`Success you made ${meal}!`)
 }
 
 
